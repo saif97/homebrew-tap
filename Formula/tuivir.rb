ccc63b55321ad6fff4acd6c62534b1774d4f3a9b
Saif <saif.hakeam97@gmail.com>
2026-09-01 17:04:31 +0000

    Update Tuivir to 0.2.0

--- Formula/tuivir.rb
+++ Formula/tuivir.rb
@@ -1,18 +1,18 @@
 class Tuivir < Formula
   desc "Inspect and operate local virtualization and container providers"
   homepage "https://github.com/saif97/Tuivir"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT OR Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/saif97/Tuivir/releases/download/v0.1.0/tuivir-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "5dd9421a93507c020ac41ea82e0a4cd1746014ef6b071c84a974db8a00a8a0d0"
+      url "https://github.com/saif97/Tuivir/releases/download/v0.2.0/tuivir-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "43590cef770d948e630d35409dabb2a89b94ffb461a49d70be8a401672e451df"
     end
 
     on_intel do
-      url "https://github.com/saif97/Tuivir/releases/download/v0.1.0/tuivir-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "04b96e18ceb2264ebb0dad091738b13d441b9abe5bbb2309b0884f2b66e6ca82"
+      url "https://github.com/saif97/Tuivir/releases/download/v0.2.0/tuivir-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "703910b55af80f82935c51df1d721a36dc05c8223e4a89eb73363dff1edc14d4"
     end
   end
 
@@ -21,6 +21,6 @@ class Tuivir < Formula
   end
 
   test do
-    assert_equal "0.1.0\n", shell_output("#{bin}/tuivir --version")
+    assert_equal "0.2.0\n", shell_output("#{bin}/tuivir --version")
   end
 end
